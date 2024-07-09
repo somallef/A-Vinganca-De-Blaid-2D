4a8d24b5a2922241e57b4677a2d48a5187998582
Allef Mizael <allef.mizael@gmail.com>
2024-07-09 15:21:26 -0300

    feat: criando um sistema de dano

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -5,24 +5,45 @@ public class MeleeEnemy : BaseEnemy
 {
     [SerializeField] private Transform detectPosition;
     [SerializeField] private Vector2 detectBoxSize;
-    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask playerLayer;    
+    [SerializeField] private float attackCooldown;
+
+    private float cooldownTimer;
 
     protected override void Update()
     {
+        cooldownTimer += Time.deltaTime;
         VerifyCanAttack();
     }
 
     private void VerifyCanAttack()
     {
+        if (cooldownTimer < attackCooldown) return;
         if (PlayerInSight())
         {
             animator.SetTrigger("attack");
+            AttackPlayer();
         }
+    }
+
+    private void AttackPlayer()
+    {
+        cooldownTimer = 0;
+        if (CheckPlayerInDetectArea().TryGetComponent(out Health playerHealth))
+        {
+            print("Making player take damage");
+            playerHealth.TakeDamage();
+        }
+    }
+
+    private Collider2D CheckPlayerInDetectArea()
+    {
+        return Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
     }
 
     private bool PlayerInSight()
     {
-        Collider2D playerCollider = Physics2D.OverlapBox(detectPosition.position, detectBoxSize, 0f, playerLayer);
+        Collider2D playerCollider = CheckPlayerInDetectArea();
         return playerCollider != null;
     }
 
--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -4,11 +4,20 @@ public class PlayerAnim : MonoBehaviour
 {
     private Animator animator;
     private IsGroundedChecker groundedChecker;
+    private Health playerHealth;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         groundedChecker = GetComponent<IsGroundedChecker>();
+        playerHealth = GetComponent<Health>();
+
+        playerHealth.OnHurt += PlayHurtAnim;
+    }
+
+    private void PlayHurtAnim()
+    {
+        animator.SetTrigger("hurt");
     }
 
     void Update()
